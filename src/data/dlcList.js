bf148c598f9bb88d197ef3bdfedc9dc30b6d17bd
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2024-10-14 17:50:15 +0300

    add data

---
+++ src/data/dlcList.js
@@ -0,0 +1,11 @@
+export const DLC = [
+  ["Orpheus", "Orpheus Picaro"],
+  ["Izanagi", "Izanagi Picaro"],
+  ["Thanatos", "Thanatos Picaro"],
+  ["Magatsu-Izanagi", "Magatsu-Izanagi Picaro"],
+  ["Kaguya", "Kaguya Picaro"],
+  ["Ariadne", "Ariadne Picaro"],
+  ["Asterius", "Asterius Picaro"],
+  ["Tsukiyomi", "Tsukiyomi Picaro"],
+  ["Messiah", "Messiah Picaro"],
+];
---
+++ src/data/items.js
@@ -0,0 +1,329 @@
+export const items = {
+  "Archangel Bra": {
+    type: "Protector",
+    user: "women",
+    description: "Def 200 / Ev 18 / +Reduce Elec dmg (high)",
+  },
+  "Arsene's Cane": {
+    type: "Weapon",
+    user: "Joker",
+    description: "Atk 130 / Acc 92 / +Random ailment (low)",
+  },
+  "Bear Gloves": {
+    type: "Weapon",
+    user: "Makoto",
+    description: "Atk 200 / Acc 90 / +Critical rate up (med)",
+  },
+  "Black Headband": {
+    type: "Accessory",
+    user: "unisex",
+    description: "+40 SP",
+  },
+  "Black Jacket": {
+    type: "Protector",
+    user: "men",
+    description: "Def 120 / Ev 12 / Prevents insta-kill from Curse",
+  },
+  "Black Moon": {
+    type: "Accessory",
+    user: "unisex",
+    description: "+Critical rate up (high)",
+  },
+  "Black Wing Robe": {
+    type: "Protector",
+    user: "unisex",
+    description: "Def 220 / Ev 20 / +Reduce Nuke dmg (med)",
+  },
+  "Brain Shot": {
+    type: "Gun",
+    user: "Ann",
+    description: "Atk 140 / Acc 84 / Rounds 12 / +Brainwash (med)",
+  },
+  Catnap: {
+    type: "Gun",
+    user: "Morgana",
+    description: "Atk 168 / Acc 90 / Rounds 5 / +Sleep (med)",
+  },
+  "Claiomh Solais - Sable": {
+    type: "Weapon",
+    user: "Morgana",
+    description: "Atk 280 / Acc 90 / +50 SP",
+  },
+  "Crystal Skull": {
+    type: "Accessory",
+    user: "unisex",
+    description: "All stats +5 / Evade Magic (high)",
+  },
+  Daffodils: {
+    type: "Accessory",
+    user: "unisex",
+    description: "+Null Despair",
+  },
+  "Darkness Ring": {
+    type: "Accessory",
+    user: "unisex",
+    description: "+Evade Curse (high)",
+  },
+  "Death Contract": {
+    type: "Weapon",
+    user: "Haru",
+    description: "Atk 140 / Acc 86 / +Despair (low)",
+  },
+  "Emperor's Amulet": {
+    type: "Accessory",
+    user: "unisex",
+    description: "En +5 / +Auto-Tarukaja",
+  },
+  "Fleur du Mal": {
+    type: "Weapon",
+    user: "Haru",
+    description: "Atk 308 / Acc 86 / Ma +5 / +Dizzy (high)",
+  },
+  "Frost Hood": {
+    type: "Protector",
+    user: "Morgana",
+    description: "Def 130 / Ev 20 / +20 SP",
+  },
+  "Hades Harp": {
+    type: "Accessory",
+    user: "unisex",
+    description: "Ag +3 / +Null Brainwash",
+  },
+  Headbutt: {
+    type: "Skillcard",
+    skillCard: true,
+  },
+  "Headhunter Ladle": {
+    type: "Weapon",
+    user: "Morgana",
+    description: "Atk 128 / Acc 90 / +Critical rate up (low)",
+  },
+  "Heaven's Gate": {
+    type: "Gun",
+    user: "Yusuke",
+    description: "Atk 378 / Acc 88 / Rounds 12 / Ag +10",
+  },
+  "Hope Diamond": {
+    type: "Accessory",
+    user: "unisex",
+    description: "All stats +3 / Regenerate 3",
+  },
+  "Hua Khon": {
+    type: "Accessory",
+    user: "unisex",
+    description: "Prevents insta-kill from Bless",
+  },
+  "Judge of the Dead": {
+    type: "Gun",
+    user: "Makoto",
+    description: "Atk 380 / Acc 92 / Rounds 6 / All stats +10",
+  },
+  "Khamrai Tao": {
+    type: "Accessory",
+    user: "unisex",
+    description: "+Reduce Fire dmg (high)",
+  },
+  "King Frost Cape": {
+    type: "Protector",
+    user: "unisex",
+    description: "Def 140 / Ev 16 / +Reduce Ice dmg (high)",
+  },
+  "Koh-i-Noor": {
+    type: "Accessory",
+    user: "unisex",
+    description: "All stats +2 / +Bless",
+  },
+  Kougaon: {
+    type: "Skillcard",
+    skillCard: true,
+  },
+  "Kuzuryu Gouhou": {
+    type: "Gun",
+    user: "Yusuke",
+    description: "Atk 300 / Acc 88 / Rounds 9 / +Random ailment (low)",
+  },
+  "Legion's Jail": {
+    type: "Accessory",
+    user: "unisex",
+    description: "+Null Fear",
+  },
+  "Life Aid": {
+    type: "Skillcard",
+    skillCard: true,
+  },
+  "Lucifer Guard": {
+    type: "Protector",
+    user: "unisex",
+    description: "Def 250 / Ev 10 / +Reduce Magic dmg (high)",
+  },
+  "Masquerade Ribbon": {
+    type: "Weapon",
+    user: "Ann",
+    description: "Atk 172 / Acc 88 / +Dizzy (low)",
+  },
+  "Megido Fire": {
+    type: "Gun",
+    user: "Ryuji",
+    description: "Atk 380 / Acc 98 / Rounds 3 / +Critical rate up (low)",
+  },
+  Mirrirmina: {
+    type: "Gun",
+    user: "Makoto",
+    description: "Atk 200 / Acc 92 / Rounds 6 / Ma +5",
+  },
+  Mjolnir: {
+    type: "Weapon",
+    user: "Ryuji",
+    description: "Atk 254 / Acc 88 / +Elec attacks",
+  },
+  "Moonlight Robe": {
+    type: "Protector",
+    user: "women",
+    description: "Def 30 / Ev 30 / +Repel Phys (high)",
+  },
+  "Naraka Whip": {
+    type: "Weapon",
+    user: "Ann",
+    description: "Atk 252 / Acc 90 / +Freeze (high)",
+  },
+  Nataraja: {
+    type: "Gun",
+    user: "Joker",
+    description: "Atk 360 / Acc 94 / Rounds 8 / All stats +5",
+  },
+  "Official's Robe": {
+    type: "Protector",
+    user: "Men",
+    description: "Def 178 / Ev 18 / +Evade Curse (high)",
+  },
+  Orlov: {
+    type: "Accessory",
+    user: "unisex",
+    description: "Ma +3 / +Repel Phys (high)",
+  },
+  "Paradise Lost": {
+    type: "Weapon",
+    user: "Joker",
+    description: "Atk 290 / Acc 92 / +Reduce Curse dmg (high)",
+  },
+  Pawzooka: {
+    type: "Gun",
+    user: "Haru",
+    description: "Atk 140 / Acc 89 / Rounds 1 / +Confuse (med)",
+  },
+  "Petra Genetrix": {
+    type: "Accessory",
+    user: "unisex",
+    description: "+Null Burn",
+  },
+  "Pumpkin Bomb": {
+    type: "Gun",
+    user: "Ryuji",
+    description: "Atk 160 / Acc 82 / Rounds 3 / +Burn (med)",
+  },
+  "Queen's Necklace": {
+    type: "Accessory",
+    user: "unisex",
+    description: "All stats +1 / +30 SP",
+  },
+  "Red Yarn Ball": {
+    type: "Accessory",
+    user: "unisex",
+    description: "Lu +10 / +Auto-Tarukaja",
+  },
+  Regent: {
+    type: "Accessory",
+    user: "unisex",
+    description: "Str +3 / +Critical rate up (med)",
+  },
+  "Ruyi Jingu Bang": {
+    type: "Weapon",
+    user: "Ryuji",
+    description: "Atk 256 / Acc 88 / +Auto-Tarukaja",
+  },
+  Sabazios: {
+    type: "Weapon",
+    user: "Makoto",
+    description: "Atk 280 / Acc 90 / +Critical rate up (high)",
+  },
+  "Senryou Yakusha": {
+    type: "Weapon",
+    user: "Yusuke",
+    description: "Atk 160 / Acc 90 / St +5",
+  },
+  Shackles: {
+    type: "Accessory",
+    user: "unisex",
+    description: "+Null Rage",
+  },
+  "Silk Dress": {
+    type: "Protector",
+    user: "women",
+    description: "Def 60 / Ev 10 / +Evade Magic (low)",
+  },
+  "Snow Queen's Whip": {
+    type: "Weapon",
+    user: "Ann",
+    description: "Atk 210 / Acc 90 / +Auto-Sukukaja",
+  },
+  "Stone of Scone": {
+    type: "Accessory",
+    user: "unisex",
+    description: "En +3 / +Reduce Phys dmg (low)",
+  },
+  Sudarshana: {
+    type: "Gun",
+    user: "Morgana",
+    description: "Atk 308 / Acc 98 / Rounds 5 / +Random ailment (low)",
+  },
+  "Tantric Oath": {
+    type: "Protector",
+    user: "men",
+    description: "Def 272 / Ev 18 / +Reduce Magic dmg (high)",
+  },
+  Tapsuan: {
+    type: "Protector",
+    user: "women",
+    description: "Def 160 / Ev 12 / +Evade Wind (high)",
+  },
+  "Thunder Band": {
+    type: "Accessory",
+    user: "unisex",
+    description: "+Null Shock",
+  },
+  "Thunder Horns": {
+    type: "Accessory",
+    user: "unisex",
+    description: "En +10 / +Fire attacks",
+  },
+  "Tyrant Pistol": {
+    type: "Gun",
+    user: "Joker",
+    description: "Atk 390 / Acc 98 / Rounds 8 / Ma +10",
+  },
+  Usumidori: {
+    type: "Weapon",
+    user: "Yusuke",
+    description: "Atk 292 / Acc 88 / +Fear (high)",
+  },
+  Vajra: {
+    type: "Weapon",
+    user: "Makoto",
+    description: "Atk 272 / Acc 90 / +Random ailment (high)",
+  },
+  "White Headband": {
+    type: "Accessory",
+    user: "unisex",
+    description: "+50 HP",
+  },
+  "Wild Hunt": {
+    type: "Gun",
+    user: "Ann",
+    description: "Atk 386 / Acc 84 / +Despair (med)",
+  },
+  Yagrush: {
+    type: "Gun",
+    user: "Haru",
+    description: "Atk 330 / Acc 80 / Rounds 1 / +Shock (med)",
+  },
+};
