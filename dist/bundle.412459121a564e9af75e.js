74de4139e42a55342b0f60fd97ba9659c500d3ac
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2024-10-14 17:47:55 +0300

    webpack setup

---
+++ dist/bundle.412459121a564e9af75e.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+console.log("Script works");
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.412459121a564e9af75e.js.map
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    bundle: path.resolve(__dirname, "src/scripts/index.js"),
+  },
+  mode: "development",
+  output: {
+    filename: "[name].[contenthash].js", // новое название с хешом при изменении файла
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      filename: "index.html",
+      template: "src/template.html", // создать шаблонный HTML в папке src
+    }),
+  ],
+};
