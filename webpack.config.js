859d756eb0302a6347a602ee1a9e7f86136eb09d
busydayhuh <107801504+busydayhuh@users.noreply.github.com>
2024-10-18 17:24:09 +0300

    added sorting and search to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,45 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    bundle: path.resolve(__dirname, "src/scripts/index.js"),
-  },
-  mode: "development",
-  output: {
-    filename: "[name].[contenthash].js", // новое название с хешом при изменении файла
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    assetModuleFilename: "[name][ext]",
-  },
-  devtool: "source-map",
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
+    entry: {
+        bundle: path.resolve(__dirname, "src/scripts/index.js"),
     },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        type: "asset/resource",
-      },
+    mode: "development",
+    output: {
+        filename: "[name].[contenthash].js", // новое название с хешом при изменении файла
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+        assetModuleFilename: "[name][ext]",
+    },
+    devtool: "source-map",
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist"),
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack App",
+            filename: "index.html",
+            favicon: "src/assets/images/favicon.ico",
+            template: "src/template.html", // создать шаблонный HTML в папке src
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack App",
-      filename: "index.html",
-      favicon: "src/assets/images/favicon.ico",
-      template: "src/template.html", // создать шаблонный HTML в папке src
-    }),
-  ],
 };
